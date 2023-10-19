262987939fe3e4df729d3cb75b0b18f89df053d9
Juvis237 <66560693+Juvis237@users.noreply.github.com>
2023-10-19 11:01:05 -0400

    Create research.py

---
+++ research.py
@@ -0,0 +1,6 @@
+import pandas
+import sarracen
+sdf = sarracen.read_phantom('/home/mbeng/Documents/DrCass/sg_00030',separate_types='all')
+
+# I do not know the format of the 'sg_00030' ; I equally do not know what the each column represents and as such I can't proceed with plots.
+# what I really need is some sort of description of the 'sg_00030' file so that I can attempt the curve plotting 
